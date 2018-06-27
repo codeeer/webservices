b768f20f0c0fd60665374e24a37a495541944ebd
codeeer <omer_olcay25@hotmail.com>
2018-06-28 01:48:24 +0300

    WCF Service Commit

--- WCFFrameworkServiceExample/WcfServiceLastSample/Properties/AssemblyInfo.cs
+++ WCFFrameworkServiceExample/WcfServiceLastSample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WCFFrameworkServiceExample")]
+[assembly: AssemblyTitle("WcfServiceLastSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WCFFrameworkServiceExample")]
+[assembly: AssemblyProduct("WcfServiceLastSample")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("95ab9093-988c-4ec8-b538-4433e2ad9d74")]
+[assembly: Guid("59cb1d4f-e0cd-492d-9122-ed907bdae743")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ WCFFrameworkServiceExample/WcfServiceLastSample/ServiceAuthenticator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.ServiceModel.Security;
+using System.Web;
+
+namespace WcfServiceLastSample
+{
+    public class ServiceAuthenticator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName))
+                throw new ArgumentNullException();
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException();
+            if(userName!= "test" || password!= "test")
+                throw new SecurityTokenException("Şifre veya paralo hatalı");
+
+        }
+    }
+}
