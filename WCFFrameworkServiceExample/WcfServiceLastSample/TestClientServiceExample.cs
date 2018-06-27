950c1285823d9bfdad4d9aa22f73169e57c3b313
codeeer <omer_olcay25@hotmail.com>
2018-06-28 02:06:29 +0300

    Correction client

--- WCFFrameworkServiceExample/WcfServiceLastSample/TestClientServiceExample.cs
+++ WCFFrameworkServiceExample/WcfServiceLastSample/TestClientServiceExample.cs
@@ -9,30 +9,30 @@ namespace WcfServiceLastSample
     {
         public void Test()
         {
-            try
-            {   
-              /*  EndpointAddress ep = new EndpointAddress(new Uri(
-                    "https://localhost:44392/Service1.svc"));
-                WSHttpBinding bind = new WSHttpBinding();
-                bind.Security.Mode = SecurityMode.TransportWithMessageCredential;
-                bind.Security.Message.ClientCredentialType =
-                    MessageCredentialType.UserName;
+            /*  try
+             {   
+                EndpointAddress ep = new EndpointAddress(new Uri(
+                     "https://localhost:44392/Service1.svc"));
+                 WSHttpBinding bind = new WSHttpBinding();
+                 bind.Security.Mode = SecurityMode.TransportWithMessageCredential;
+                 bind.Security.Message.ClientCredentialType =
+                     MessageCredentialType.UserName;
 
 
-                Service1Client client = new Service1Client(bind, ep);
-                //      client.ClientCredentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindByIssuerName, "<issuername>");
+                 Service1Client client = new Service1Client(bind, ep);
+                 //      client.ClientCredentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindByIssuerName, "<issuername>");
 
-                client.ClientCredentials.UserName.UserName = "test";
-                client.ClientCredentials.UserName.Password = "test";
-                Console.WriteLine(client.GetData(1) + "  ");
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                Console.ReadLine();
-                throw;
-            }        */
+                 client.ClientCredentials.UserName.UserName = "test";
+                 client.ClientCredentials.UserName.Password = "test";
+                 Console.WriteLine(client.GetData(1) + "  ");
+                 Console.ReadLine();
+             }
+             catch (Exception e)
+             {
+                 Console.WriteLine(e);
+                 Console.ReadLine();
+                 throw;
+             }        */
         }
     }
 }
